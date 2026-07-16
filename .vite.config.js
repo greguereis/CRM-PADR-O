87c2ebbd0228d0422f33b17845638ae4d9cdcfc6
greguereis <greguereisferreira@gmail.com>
2026-07-16 18:31:48 -0300

    5

--- .vite.config.js
+++ .vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     open: true,
-    host: true, // Permite acesso de dispositivos na mesma rede
-    strictPort: false, // Se a porta 3000 estiver ocupada, tenta outra
+    host: true,
+    strictPort: false,
   },
   build: {
     outDir: 'dist',
